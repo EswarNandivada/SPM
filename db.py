748340195c74d9c0d50e9cd779a5a5dc8d3f3ff6
EswarNandivada <posieswarnandivada@gmail.com>
2023-04-12 19:34:59 +0530

    first

--- db.py
+++ db.py
@@ -6,7 +6,8 @@ password=os.environ['RDS_PASSWORD']
 host=os.environ['RDS_HOSTNAME']
 port=os.environ['RDS_PORT']
 def dbconn():
-    with mysql.connector.connect(host=host,user=user,password=password,db=db,port=port) as conn:
+    mysql_con=mysql.connector.connect(host=host,user=user,password=password,db=db,port=port)
+    with mysql_con as conn:
         cursor=conn.cursor()
         cursor.execute('create table if not exists students(rollno varchar(6) primary key,name varchar(30),std_group varchar(10),password varchar(15),email varchar(70))')
         cursor.execute('create table if not exists notes(nid int primary key auto_increment,rollno varchar(6),title varchar(30),content text,date datetime default now(),foreign key(rollno) references students(rollno))')
