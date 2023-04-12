2d9a4830e30913657066716054e7dea5ba4eb33d
EswarNandivada <posieswarnandivada@gmail.com>
2023-04-12 20:07:36 +0530

    finished

--- app.py
+++ app.py
@@ -17,7 +17,11 @@ host=os.environ['RDS_HOSTNAME']
 port=os.environ['RDS_PORT']
 mydb=mysql.connector.connect(host=host,user=user,password=password,db=db,port=port)
 #mydb=mysql.connector.connect(host='localhost',user='root',password='Eswar@2001',db='spm')
-
+with mysql.connector.connect(host=host,user=user,password=password,db=db,port=port) as conn:
+    cursor=conn.cursor()
+    cursor.execute('create table if not exists students(rollno varchar(6) primary key,name varchar(30),std_group varchar(10),password varchar(15),email varchar(70))')
+    cursor.execute('create table if not exists notes(nid int primary key auto_increment,rollno varchar(6),title varchar(30),content text,date datetime default now(),foreign key(rollno) references students(rollno))')
+    cursor.execute('create table if not exists files(fid int primary key auto_increment,rollno varchar(6),filename varchar(30),filedata longblob,date datetime default now(),foreign key(rollno) references students(rollno))')
 Session(app)
 @app.route('/')
 def index():
@@ -261,8 +265,6 @@ def createpassword():
     else:
         return redirect(url_for('login'))
 if __name__=="__main__":
-    dbconn()
-    print('hello')
     app.run()
 
 
